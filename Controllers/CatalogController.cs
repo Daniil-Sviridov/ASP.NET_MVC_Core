cadf7a8d0ed890ace04956c2bc64c2a8f75b28cb
Daniil Sviridov <daniil_sviridov@mail.ru>
2022-06-01 14:39:24 +0300

    Add project files.

---
+++ Controllers/CatalogController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_study.Models;
+
+namespace MVC_study.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private Catalog _catalog = new();
+
+        public CatalogController()
+        {
+            _catalog.Products.Add(new Product() { Id = 1, Name = "Тест" }) ;
+        }
+
+        [HttpGet]
+        public IActionResult Products()
+        {
+            return View(_catalog);
+        }
+
+        [HttpPost]
+        public IActionResult Products(Product model)
+        {
+            _catalog.Products.Add(model);
+            return View(_catalog);
+        }
+    }
+}
---
+++ Models/Catalog.cs
@@ -0,0 +1,7 @@
+﻿namespace MVC_study.Models
+{
+    public class Catalog
+    {
+        public List<Product> Products { get; set; } = new();
+    }
+}
