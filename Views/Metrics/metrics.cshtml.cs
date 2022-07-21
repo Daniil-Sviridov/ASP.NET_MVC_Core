c7fef9df9ebba8fa80447576cd1a61a7b754bca1
Daniil Sviridov <daniil_sviridov@mail.ru>
2022-07-21 18:59:38 +0300

    Страница метрик

---
+++ Views/Metrics/metrics.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVC_study.Views
+{
+    public class metricsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
